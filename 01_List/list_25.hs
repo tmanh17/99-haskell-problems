7b159767bc9e4a0fc71920110d0df6e7730c56a8
manhdt <manhdt@manhdts-MacBook-Pro.local>
2023-03-02 19:58:13 +0700

    solve 25

--- 01_List/list_25.hs
+++ 01_List/list_25.hs
@@ -12,5 +12,21 @@ Example in Haskell:
 
 -}
 
+import System.Random (randomRIO)
+
+removeAt :: (Eq a) => Int -> [a] -> [a]
+removeAt _ [] = []
+removeAt k (x:xs)
+    | k == 0 = removeAt (k-1) xs
+    | otherwise = x:(removeAt (k-1) xs)
+
+rnd_permu :: (Eq a) => [a] -> IO [a]
+rnd_permu [] = return []
+rnd_permu xs = 
+    do
+        idx <- randomRIO (0, (length xs)-1)
+        rest <- rnd_permu (removeAt idx xs)
+        return ((xs!!idx):rest)
+
 
 
--- 01_List/list_26.hs
+++ 01_List/list_26.hs
@@ -14,5 +14,3 @@ Example in Haskell:
 
 -}
 
-
-
