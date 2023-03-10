06100ec5ee13430b328faca2e5249a912092943e
manhdt <tmanh17@gmail.com>
2023-03-10 19:18:36 +0700

    solve 82_cycle

--- 06_Graphs/82_cycle.hs
+++ 06_Graphs/82_cycle.hs
@@ -10,3 +10,31 @@ Example in Haskell:
 λ> cycle 1 [(1,2),(2,3),(1,3),(3,4),(4,2),(5,6)]
 []
 -}
+
+
+-- remove edge
+
+
+import Graph
+
+
+myCycle :: Int -> [(Int, Int)] -> [[Int]]
+myCycle s edges = cycle' s s edges 0
+    where 
+        cycle' s t edges visits
+            | length edges == 0  = []
+            | s == t && visits /= 0 = [[t]] 
+            | otherwise = [s:path | edge <- edges, path <- (cycle' (snd edge) t [e|e <- edges, e /= edge] (visits+1)), (fst edge) == s, s /= t || visits == 0]
+                
+
+
+--  import Data.List (partition)
+--  cycle' :: Int -> [(Int, Int)] -> [ [Int] ]
+--  cycle' n g = search [[n]] []
+--    where search [] result = result
+--          search cur result = search (go active) (arrive ++ result)
+--            where split = partition end cur
+--                  end s = (last s == n) && (length s /= 1)
+--                  active = snd split
+--                  arrive = fst split
+--                  go ls = [ x ++ [snd y] | x <- ls, y <- g, last x == fst y, not (snd y `elem` tail x)]
