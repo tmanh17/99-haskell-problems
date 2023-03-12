bc0bc62df30772b80b61dc9eeb201e3a192496f9
manhdt <tmanh17@gmail.com>
2023-03-12 16:40:12 +0700

    solve 88 connected components

--- 06_Graphs/88_connectedComponents.hs
+++ 06_Graphs/88_connectedComponents.hs
@@ -8,3 +8,42 @@ Example in Haskell:
 λ> connectedComponents ([1,2,3,4,5,6,7], [(1,2),(2,3),(1,4),(3,4),(5,2),(5,4),(6,7)])
 [[1,2,3,4,5][6,7]]
 -}
+
+
+import Data.List
+
+type Node = Int
+type Edge = (Node,Node)
+type Graph = ([Node],[Edge])
+
+dfs :: Graph -> Node -> [Node]
+dfs (nodes, edges) c = dfs' (nodes, edges) [c]
+    where 
+        dfs' ([],_) _ = []
+        dfs' (_,_) [] = []
+        dfs' (nodes, edges) (top:stack)
+            | [x|x<-nodes, x==top] == [] = dfs' (newNodes, edges) stack
+            | otherwise = top : dfs' (newNodes, edges) (adjacent ++ stack)
+                where
+                    adjacent = [x | (x,y)<-edges,y==top] ++ [x | (y,x)<-edges,y==top]
+                    newNodes = [x|x<-nodes, x/=top]
+
+connectedComponents :: Graph -> [[Node]]
+connectedComponents ([],_) = []
+connectedComponents (x:nodes, edges) = connected : (connectedComponents (newNodes, edges))
+    where 
+        connected = dfs (x:nodes, edges) x
+        newNodes = filter (\a -> not $ a `elem` connected) nodes
+
+connectedcomponents' :: Graph -> [[Node]]
+connectedcomponents' ([],_) = []
+connectedcomponents' (top:v,e) 
+    | remaining == [] = [connected]
+    | otherwise = connected : connectedcomponents' (remaining, e)
+        where
+            connected = dfs (top:v,e) top
+            remaining = (top:v) \\ connected
+
+
+
+            
